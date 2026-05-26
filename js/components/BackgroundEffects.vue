72b01af6ac9bbdc9cc03be182cc01185777fbcdd
finn-9800x3d <oscar861213@gmail.com>
2026-05-26 21:51:48 +0800

    feat(js): 章魚燒改用多方向隨機飄移動畫（含旋轉與 X/Y 位移）

--- js/components/BackgroundEffects.vue
+++ js/components/BackgroundEffects.vue
@@ -11,17 +11,52 @@
     <div class="bg-decor animate-spin-reverse" style="top:20vh; right:2vw; width:50px; height:50px; border-radius:12px; background:#F7C94822; transform-origin:center;"></div>
 
     <!-- 章魚燒裝飾 -->
-    <div class="bg-decor animate-float-slow"   style="top:42vh; right:3vw;  width:90px;  opacity:0.22;">
-      <img :src="'public/images/views/章魚燒2.png'" alt="" style="width:100%; transform:rotate(12deg);" />
+    <div class="bg-decor tako-wander-1" style="top:42vh; right:3vw;  width:90px;  opacity:0.22;">
+      <img :src="'public/images/views/章魚燒2.png'" alt="" style="width:100%;" />
     </div>
-    <div class="bg-decor animate-float-medium" style="top:8vh;  left:42vw;  width:70px;  opacity:0.18;">
-      <img :src="'public/images/views/章魚燒2.png'" alt="" style="width:100%; transform:rotate(-8deg);" />
+    <div class="bg-decor tako-wander-2" style="top:8vh;  left:42vw;  width:70px;  opacity:0.18;">
+      <img :src="'public/images/views/章魚燒2.png'" alt="" style="width:100%;" />
     </div>
-    <div class="bg-decor animate-float-fast"   style="top:68vh; left:2vw;   width:110px; opacity:0.20;">
-      <img :src="'public/images/views/章魚燒1.png'" alt="" style="width:100%; transform:rotate(6deg);" />
+    <div class="bg-decor tako-wander-3" style="top:68vh; left:2vw;   width:110px; opacity:0.20;">
+      <img :src="'public/images/views/章魚燒1.png'" alt="" style="width:100%;" />
     </div>
-    <div class="bg-decor animate-float-slow"   style="top:15vh; right:1vw;  width:75px;  opacity:0.16;">
-      <img :src="'public/images/views/章魚燒2.png'" alt="" style="width:100%; transform:rotate(-15deg);" />
+    <div class="bg-decor tako-wander-4" style="top:15vh; right:1vw;  width:75px;  opacity:0.16;">
+      <img :src="'public/images/views/章魚燒2.png'" alt="" style="width:100%;" />
     </div>
   </div>
 </template>
+
+<style>
+@keyframes takoWander1 {
+  0%   { transform: translate(0px,   0px)   rotate(0deg);   }
+  20%  { transform: translate(18px, -28px)  rotate(8deg);   }
+  45%  { transform: translate(-10px, -45px) rotate(-4deg);  }
+  70%  { transform: translate(22px,  -18px) rotate(12deg);  }
+  100% { transform: translate(0px,   0px)   rotate(0deg);   }
+}
+@keyframes takoWander2 {
+  0%   { transform: translate(0px,   0px)   rotate(0deg);   }
+  30%  { transform: translate(-20px, -30px) rotate(-10deg); }
+  55%  { transform: translate(14px,  -50px) rotate(6deg);   }
+  80%  { transform: translate(-8px,  -20px) rotate(-5deg);  }
+  100% { transform: translate(0px,   0px)   rotate(0deg);   }
+}
+@keyframes takoWander3 {
+  0%   { transform: translate(0px,   0px)   rotate(0deg);   }
+  25%  { transform: translate(25px,  -15px) rotate(5deg);   }
+  50%  { transform: translate(10px,  -38px) rotate(-8deg);  }
+  75%  { transform: translate(-15px, -22px) rotate(10deg);  }
+  100% { transform: translate(0px,   0px)   rotate(0deg);   }
+}
+@keyframes takoWander4 {
+  0%   { transform: translate(0px,   0px)   rotate(0deg);   }
+  35%  { transform: translate(-12px, -35px) rotate(-12deg); }
+  65%  { transform: translate(20px,  -20px) rotate(7deg);   }
+  100% { transform: translate(0px,   0px)   rotate(0deg);   }
+}
+
+.tako-wander-1 { animation: takoWander1 8s  ease-in-out infinite; }
+.tako-wander-2 { animation: takoWander2 11s ease-in-out infinite 1.5s; }
+.tako-wander-3 { animation: takoWander3 9s  ease-in-out infinite 3s;   }
+.tako-wander-4 { animation: takoWander4 13s ease-in-out infinite 0.8s; }
+</style>
